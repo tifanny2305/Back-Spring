1f5aa789df260410731a9615e845c2a385973830
RafaelSvgh <salvatierravallerafael@gmail.com>
2024-06-09 21:04:10 -0400

    creacion de backend

--- src/main/java/com/examen/ExamenApplication.java
+++ src/main/java/com/examen/ExamenApplication.java
@@ -1,17 +1,9 @@
 package com.examen;
 
-import com.examen.entity.ERole;
-import com.examen.entity.RoleEntity;
-import com.examen.entity.Usuario;
-import com.examen.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Set;
 
 @SpringBootApplication
 public class ExamenApplication {
